2b5d167a2e19752e6310338d8c9eba294d4f8d23
Vinicius-Mance <vinimanceprof@gmail.com>
2021-06-09 20:28:51 -0300

    pagamento: máscaras e patterns adicionados

--- js/index.js
+++ js/index.js
@@ -1,27 +1,70 @@
 try {
 
 let numero = document.getElementById('numero');
-console.log(numero);
 let nome = document.getElementById('nome');
 let cvv = document.getElementById('cvv');
 let validade = document.getElementById('validade');
 
-numero.onchange = function () { infoCartaoVisual(numero,numeroCartaoVisual) }
+numero.onkeyup = function () { infoCartaoNumero(numero,numeroCartaoVisual) }
 
-nome.onchange = function () { infoCartaoVisual(nome,nomeCartaoVisual) }
+nome.onkeyup = function () { infoCartaoNome(nome,nomeCartaoVisual) }
 
-cvv.onchange = function () { infoCartaoVisual(cvv,cvvCartaoVisual) }
+cvv.onkeyup = function () { infoCartaoCvv(cvv,cvvCartaoVisual) }
 
-validade.onchange = function () { infoCartaoVisual(validade,validadeCartaoVisual) }
+validade.onkeyup = function () { infoCartaoValidade(validade,validadeCartaoVisual) }
 
 let numeroCartaoVisual = document.getElementById('numeroCartaoVisual');
 let nomeCartaoVisual = document.getElementById('nomeCartaoVisual');
 let cvvCartaoVisual = document.getElementById('cvvCartaoVisual');
 let validadeCartaoVisual = document.getElementById('validadeCartaoVisual');
 
-const infoCartaoVisual = (input,campoCartao) => {
-  campoCartao.innerHTML = input.value;
+function infoCartaoNumero (campo,campoCartao) {
+        campo.value = campo.value
+        .replace(/\D+/g, '')
+        .replace(/(\d{4})(\d)/, '$1 $2')
+        .replace(/(\d{4})(\d)/, '$1 $2')
+        .replace(/(\d{4})(\d)/, '$1 $2')
+        .replace(/(\d{4})\d+?$/, '$1');
+
+        if (campo.value.length == 0) {
+          campoCartao.innerHTML = "0000 0000 0000 0000";
+        } else {
+          campoCartao.innerHTML = campo.value;
+        }
 }
 
+function infoCartaoNome (campo,campoCartao) {
+      campo.value = campo.value
+          .replace(/\d+/g, '');
+            if (campo.value.length == 0) {
+              campoCartao.innerHTML = "Jhon Doe";
+            } else {
+              campoCartao.innerHTML = campo.value;
+            }
+}
+
+function infoCartaoValidade (campo,campoCartao) {
+    campo.value = campo.value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{2})(\d)/, '$1');
+
+        if (campo.value.length == 0) {
+          campoCartao.innerHTML = "12/21";
+        } else {
+          campoCartao.innerHTML = campo.value;
+        }
+
+}
+
+function infoCartaoCvv(campo,campoCartao) {
+      campo.value = campo.value.replace(/\D+/g, '');
+      if (campo.value.length == 0) {
+          campoCartao.innerHTML = "123";
+      } else {
+          campoCartao.innerHTML = campo.value;
+      }
+
+}
 
 } catch (e) {}
