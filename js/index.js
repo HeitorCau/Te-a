cd7d871b0a789b4699726f9b68de548bd6b1d52e
Vinicius-Mance <vinimanceprof@gmail.com>
2021-06-09 22:54:28 -0300

    pagamento: validações e mensagens de erro adicionadas

--- js/index.js
+++ js/index.js
@@ -5,20 +5,32 @@ let nome = document.getElementById('nome');
 let cvv = document.getElementById('cvv');
 let validade = document.getElementById('validade');
 
-numero.onkeyup = function () { infoCartaoNumero(numero,numeroCartaoVisual) }
+let formPagamento = document.getElementById('form-pagamento');
 
-nome.onkeyup = function () { infoCartaoNome(nome,nomeCartaoVisual) }
+formPagamento.onsubmit = function (e) {e.preventDefault(); verificarCamposPagamento(nome, nomeErro, numero, numeroErro, validade, validadeErro, cvv, cvvErro);}
 
-cvv.onkeyup = function () { infoCartaoCvv(cvv,cvvCartaoVisual) }
+numero.onkeyup = function () { infoCartaoNumero(numero,numeroCartaoVisual,numeroErro) }
 
-validade.onkeyup = function () { infoCartaoValidade(validade,validadeCartaoVisual) }
+nome.onkeyup = function () { infoCartaoNome(nome,nomeCartaoVisual,nomeErro) }
+
+cvv.onkeyup = function () { infoCartaoCvv(cvv,cvvCartaoVisual,cvvErro) }
+
+validade.onkeyup = function () { infoCartaoValidade(validade,validadeCartaoVisual,validadeErro) }
 
 let numeroCartaoVisual = document.getElementById('numeroCartaoVisual');
 let nomeCartaoVisual = document.getElementById('nomeCartaoVisual');
 let cvvCartaoVisual = document.getElementById('cvvCartaoVisual');
 let validadeCartaoVisual = document.getElementById('validadeCartaoVisual');
 
-function infoCartaoNumero (campo,campoCartao) {
+let numeroErro = document.getElementById('numero-erro');
+let nomeErro = document.getElementById('nome-erro');
+let validadeErro = document.getElementById('validade-erro');
+let cvvErro = document.getElementById('cvv-erro');
+
+function infoCartaoNumero (campo,campoCartao,campoErro) {
+
+        campoErro.innerHTML = "";
+
         campo.value = campo.value
         .replace(/\D+/g, '')
         .replace(/(\d{4})(\d)/, '$1 $2')
@@ -33,7 +45,10 @@ function infoCartaoNumero (campo,campoCartao) {
         }
 }
 
-function infoCartaoNome (campo,campoCartao) {
+function infoCartaoNome (campo,campoCartao,campoErro) {
+
+      campoErro.innerHTML = "";
+
       campo.value = campo.value
           .replace(/\d+/g, '');
             if (campo.value.length == 0) {
@@ -43,7 +58,10 @@ function infoCartaoNome (campo,campoCartao) {
             }
 }
 
-function infoCartaoValidade (campo,campoCartao) {
+function infoCartaoValidade (campo,campoCartao,campoErro) {
+
+    campoErro.innerHTML = "";
+
     campo.value = campo.value
         .replace(/\D+/g, '')
         .replace(/(\d{2})(\d)/, '$1/$2')
@@ -57,13 +75,36 @@ function infoCartaoValidade (campo,campoCartao) {
 
 }
 
-function infoCartaoCvv(campo,campoCartao) {
+function infoCartaoCvv(campo,campoCartao,campoErro) {
+
+      campoErro.innerHTML = "";
+
       campo.value = campo.value.replace(/\D+/g, '');
       if (campo.value.length == 0) {
           campoCartao.innerHTML = "123";
       } else {
           campoCartao.innerHTML = campo.value;
       }
+
+}
+
+function verificarCamposPagamento(nome, nomeErro, numero, numeroErro, validade, validadeErro, cvv, cvvErro){
+
+  if (nome.value.length == 0) {
+    nomeErro.innerHTML = "Insira o nome no cartão";
+  }
+
+  if (numero.value.length == 0) {
+    numeroErro.innerHTML = "Insira o numero do cartão";
+  }
+
+  if (validade.value.length == 0) {
+    validadeErro.innerHTML = "Insira o validade do cartão";
+  }
+
+  if (cvv.value.length == 0) {
+    cvvErro.innerHTML = "Insira o código de validação do cartão";
+  }
 
 }
 
